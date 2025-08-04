58173be37189c8e9aaa479e80831fa4426ca54c8
Aaditya Muleva <aaditya@Aadityas-MacBook-Pro.local>
2025-08-04 20:23:20 +0530

    test deply app

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 users = [
     {"id": 1, "name": "John Doe", "email": "john@example.com", "version":"v3"},
     {"id": 2, "name": "Jane Smith", "email": "jane@example.com", "version":"v3"},
+    {"id": 3, "name": "Jane dsd", "email": "jane@example.com", "version":"v3"},
 ]
 
 @app.route('/health', methods=['GET'])
